7788552940c38e4048e9f90f323d5bed6f170ca1
hukhan19962012 <nidalee002@gmail.com>
2020-10-23 17:03:17 +0700

    ss

---
+++ android/app/src/main/kotlin/com/example/SMSAndroid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.SMSAndroid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://cdnmedia.webthethao.vn/uploads/img/files/images/fullsize/ao-shin.jpg'),
+              ),
+              Text(
+                'Cuong',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
