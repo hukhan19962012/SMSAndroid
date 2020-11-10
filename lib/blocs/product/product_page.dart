4470e423e3aa93ff8fb376276287700534194670
hukhan19962012 <nidalee002@gmail.com>
2020-11-10 21:08:12 +0700

    dd
    dd

--- lib/blocs/product/product_page.dart
+++ lib/blocs/product/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:SMSAndroid/models/product.dart';
+import 'package:SMSAndroid/repository/product_repository.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:SMSAndroid/blocs/product/index.dart';
@@ -51,29 +52,3 @@ class _ProductPageState extends State<ProductPage> {
   
 
 }
-  class DataSearch extends SearchDelegate<Product>{
-  @override
-  List<Widget> buildActions(BuildContext context) {
-      // TODO: implement buildActions
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildLeading(BuildContext context) {
-      // TODO: implement buildLeading
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildResults(BuildContext context) {
-      // TODO: implement buildResults
-      throw UnimplementedError();
-    }
-  
-    @override
-    Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
-  }
-    
-  }
